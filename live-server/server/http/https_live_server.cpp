f955798089009b3c259755fbb52cc925e9063017
root <root@localhost.localdomain>
2025-05-21 22:50:33 +0800

    http服务器的路由树，去掉尾部的.的处理，这样感觉会更清晰一些，由此，需要修改http服务的一些路径

--- live-server/server/http/https_live_server.cpp
+++ live-server/server/http/https_live_server.cpp
@@ -37,7 +37,7 @@ std::shared_ptr<SSL_CTX> HttpsLiveServer::on_tls_ext_servername(const std::strin
 
 bool HttpsLiveServer::register_route() {
     bool ret;
-    ret = on_get("/:app/:stream.flv", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
+    ret = on_get("/:app/:stream/live.flv", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
         (void)session;
         auto http_flv_session = std::make_shared<HttpFlvServerSession>(req, resp);
         http_flv_session->service();
@@ -48,7 +48,7 @@ bool HttpsLiveServer::register_route() {
         return false;
     }
 
-    ret = on_get("/:app/:stream.m3u8", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
+    ret = on_get("/:app/:stream/live.m3u8", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
         (void)session;
         auto http_m3u8_session = std::make_shared<HttpM3u8ServerSession>(req, resp);
         http_m3u8_session->service();
@@ -59,7 +59,7 @@ bool HttpsLiveServer::register_route() {
         return false;
     }
 
-    ret = on_get("/:app/:stream/:id.ts", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
+    ret = on_get("/:app/:stream/live/ts/:filename", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
         (void)session;
         auto http_ts_session = std::make_shared<HttpTsServerSession>(req, resp);
         http_ts_session->service();
@@ -70,7 +70,7 @@ bool HttpsLiveServer::register_route() {
         return false;
     }
 
-    ret = on_get("/:app/:stream.ts", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
+    ret = on_get("/:app/:stream/live.ts", [](std::shared_ptr<HttpServerSession> session, std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)->boost::asio::awaitable<void> {
         (void)session;
         auto http_long_ts_session = std::make_shared<HttpLongTsServerSession>(req, resp);
         http_long_ts_session->service();
